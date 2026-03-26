36d10723ced3a2a0c119d65213cb409ebcf867e0
Serena <sere.michetti@icloud.com>
2026-03-26 16:37:56 +0100

    installo react, elimino i file e le immagini non necessari; creo un README con i passaggi da effettuare; instanno react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+//import './App.css'
+
+function App() {
+ // const [count, setCount] = useState(null)
+
+  return (
+    <>
+      <h1>Web App React</h1>
+    </>
+  )
+}
+
+export default App
