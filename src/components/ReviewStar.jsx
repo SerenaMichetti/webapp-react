c484800f04d6f39bad09814b6253f68605fcec8e
Serena <sere.michetti@icloud.com>
2026-03-31 16:46:17 +0200

    commit precedente sbagliata

--- src/components/ReviewStar.jsx
+++ src/components/ReviewStar.jsx
@@ -1,11 +1,11 @@
 function ReviewStar({voteParam}) {
 
     return <div className="vote" >
-        {voteParam >= 1 ? <i class="fa-solid fa-star"></i> : <i class="fa-regular fa-star"></i>}
-        {voteParam >= 2 ? <i class="fa-solid fa-star"></i> : <i class="fa-regular fa-star"></i>}
-        {voteParam >= 3 ? <i class="fa-solid fa-star"></i> : <i class="fa-regular fa-star"></i>}
-        {voteParam >= 4 ? <i class="fa-solid fa-star"></i> : <i class="fa-regular fa-star"></i>}
-        {voteParam >= 5 ? <i class="fa-solid fa-star"></i> : <i class="fa-regular fa-star"></i>}
+        {voteParam >= 1 ? <i className="fa-solid fa-star"></i> : <i className="fa-regular fa-star"></i>}
+        {voteParam >= 2 ? <i className="fa-solid fa-star"></i> : <i className="fa-regular fa-star"></i>}
+        {voteParam >= 3 ? <i className="fa-solid fa-star"></i> : <i className="fa-regular fa-star"></i>}
+        {voteParam >= 4 ? <i className="fa-solid fa-star"></i> : <i className="fa-regular fa-star"></i>}
+        {voteParam >= 5 ? <i className="fa-solid fa-star"></i> : <i className="fa-regular fa-star"></i>}
     </div>
 }
 
